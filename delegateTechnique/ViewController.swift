25c1b9accd9178d96e8ca0baab8b61e907804740
Mohamed Samir <mohamedsamir@Mohameds-MacBook-Air.local>
2019-07-27 08:50:44 -0700

    Initial Commit

---
+++ delegateTechnique/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  delegateTechnique
+//
+//  Created by Mohamed Samir on 7/27/19.
+//  Copyright © 2019 Mohamed Samir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
